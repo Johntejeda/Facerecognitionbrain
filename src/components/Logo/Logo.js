b4cf7c0b44adb006bc3c02ce82e44333f36790f5
Johntejeda <Johntejeda1969@gmail.com>
2018-09-03 21:01:05 -0500

    upload

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = () => {
+  return (<div className='ma4 mt0'>
+    <Tilt className="Tilt br3 shadow-3" options={{
+        max: 55
+      }} style={{
+        height: 150,
+        width: 150
+      }}>
+      <div className="Tilt-inner p5">
+       <img style={{paddingTop: '20px'}} alt='logo' Src={brain}/>
+      </div>
+    </Tilt>
+
+  </div>);
+}
+
+export default Logo;
